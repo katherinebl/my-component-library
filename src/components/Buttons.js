fe33ca4148e6b6e4c84bb38f6e6d565fe5732583
Katherine <katherine.briceno@zooplus.com>
2023-04-01 22:22:19 +0200

    Added hover state to buttons

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -7,26 +7,42 @@ const Button = styled.button`
   color: white;
   border-radius: 5px;
   border: none;
-  min-width: 130px;
+  min-width: 150px;
   width: auto;
   cursor: pointer;
   display: block;
   margin-bottom: 15px;
+
+  &:hover {
+    transition: background-color 0.2s, color 0.2s, filter 0.2s;
+  }
 `
 
 export const PrimaryButton = styled(Button)`
   background-color: ${defaultTheme.primaryColor};
 
+  &:hover {
+    filter: drop-shadow(0 2px 5px ${defaultTheme.primaryColor});
+  }
 `
 
 export const SecondaryButton = styled(Button)`
   border: solid 2px ${defaultTheme.primaryColorActive};
   color: ${defaultTheme.primaryColorActive};
-  background: ${defaultTheme.whiteColor} ;
+  background-color: ${defaultTheme.whiteColor} ;
+
+  &:hover {
+    background-color: ${defaultTheme.primaryColorActive};
+    color: ${defaultTheme.whiteColor};
+  }
 `;
 
 export const TertiaryButton = styled(Button)`
   background-color: ${defaultTheme.primaryColorHover};
+
+  &:hover {
+    filter: drop-shadow(0 2px 5px ${defaultTheme.primaryColorHover});
+  }
 `;
 
 
