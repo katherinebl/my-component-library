e5608b7d58ccfeba6a33957c8c9aacb471da6288
Katherine <katherine.briceno@zooplus.com>
2023-04-01 22:00:59 +0200

    Added global styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { PrimaryButton, SecondaryButton, TertiaryButton } from './components/Buttons.js';
+import { GlobalStyle } from './utils/Global.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -8,6 +9,8 @@ root.render(
     <PrimaryButton>Primary</PrimaryButton>
     <SecondaryButton>Secondary</SecondaryButton>
     <TertiaryButton>Terciary</TertiaryButton>
+
+    <GlobalStyle />
   </React.StrictMode>
 );
 
