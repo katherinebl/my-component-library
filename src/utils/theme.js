faf751c788b9d1f4a5b94f11ae365c7f9653c199
Katherine <katherine.briceno@zooplus.com>
2023-04-01 20:26:35 +0200

    Added color, typography & theme utilities

---
+++ src/utils/theme.js
@@ -0,0 +1,11 @@
+import { blue, neutral } from './colors'
+import { primaryFont } from './typography'
+
+export const defaultTheme = {
+  primaryColor: blue[400],
+  primaryColorHover: blue[100],
+  primaryColorActive: blue[500],
+  whiteText: neutral[100],
+  blackText: neutral[600],
+  primaryFont
+}
